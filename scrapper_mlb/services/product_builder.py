d26a93c4fad735fc3d7ac904fdf7636a76f47fae
TrveLeo <leandro.baldan.ferreira@gmail.com>
2026-01-29 15:24:56 -0300

    feat: update build_product.py and

--- scrapper_mlb/services/product_builder.py
+++ scrapper_mlb/services/product_builder.py
@@ -1,12 +1,20 @@
-from scrapper_mlb.services.extractors.link import extract_link
-from scrapper_mlb.services.extractors.rating import extract_rating
-from scrapper_mlb.services.extractors.buyers import extract_buyers
-from scrapper_mlb.services.extractors.shipping import extract_shipping
+from models import Product
+from services.extractors.description import extract_description
+from services.extractors.link import extract_link
+from services.extractors.product_id import extract_product_id
+from services.extractors.price import extract_price
+from services.extractors.rating import extract_rating
+from services.extractors.image import extract_image
 
 def build_product(item):
-    return {
-        'link': extract_link(item),
-        'avaliacao': extract_rating(item),
-        'compradores': extract_buyers(item),
-        'frete': extract_shipping(item),
-    }
+    link = extract_link(item)
+
+    return Product(
+        id_produto=extract_product_id(link),
+        descricao=extract_description(item),
+        preco=extract_price(item),
+        avaliacao=extract_rating(item),
+        desconto=None,
+        link=link,
+        imagem_url=extract_image(item)
+    )
