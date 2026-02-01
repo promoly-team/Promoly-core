5a13cb26084c268f5d286c6b1407ee9a7f6409db
TrveLeo <leandro.baldan.ferreira@gmail.com>
2026-01-31 22:40:27 -0300

    feat: add categories

--- scrapper_mlb/config.py
+++ scrapper_mlb/config.py
@@ -9,3 +9,27 @@ HEADERS = {
 
 CRED_JSON = "creds.json"
 SHEET_NAME = "request"
+
+CATEGORIES = {
+    "eletronicos": [
+        "fone bluetooth",
+        "caixa de som bluetooth",
+        "power bank",
+        "carregador usb c",
+        "smartwatch",
+    ],
+    "casa": [
+        "organizador multiuso",
+        "luminária led",
+        "torneira gourmet",
+        "suporte para tv",
+        "escorredor de louça",
+    ],
+    "pet": [
+        "comedouro automático pet",
+        "bebedouro automático pet",
+        "cama pet",
+        "brinquedo para cachorro",
+        "coleira para cachorro",
+    ],
+}
