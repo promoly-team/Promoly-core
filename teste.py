dd21005fac5f018a0eae28a5cd0bd317bcebfbd6
TrveLeo <leandro.baldan.ferreira@gmail.com>
2026-01-31 22:41:50 -0300

    só testando umas coisas aqui pro futuro, pode ignorar

--- teste.py
+++ teste.py
@@ -1,16 +1,24 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-url = "http://localhost:3000/api/sendText"
+load_dotenv()
+
+WAHA_API_KEY = os.getenv("WAHA_API_KEY")
+
+url = "http://waha:3000/api/sendText"
+
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/json",
-    "X-Api-Key": ""
+    "X-Api-Key": WAHA_API_KEY
 }
+
 data = {
-    "chatId": "update@c.us",
+    "chatId": "5527996956042@c.us",
     "text": "Hi there!",
-    "session": "default"
+    "session": "default" 
 }
 
 response = requests.post(url, json=data, headers=headers)
-print(response.json())
+print(response.status_code, response.text)
