c845d176dfe51e6bd685d4343d85441fc2c9bcae
TrveLeo <leandro.baldan.ferreira@gmail.com>
2026-02-08 12:39:42 -0300

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,14 +3,32 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <Link to="/">Promoly</Link>
+    <header className="navbar">
+      <div className="navbar__container">
+        <Link to="/" className="navbar__brand">
+          Promoly
+        </Link>
 
-      <div className="nav-links">
-        <Link to="/categoria/eletronicos">Eletrônicos</Link>
-        <Link to="/categoria/casa">Casa</Link>
-        <Link to="/categoria/pet">Pet</Link>
+        <nav className="navbar__menu">
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <Link className="navbar__link" to="/categoria/eletronicos">
+                Eletrônicos
+              </Link>
+            </li>
+            <li className="navbar__item">
+              <Link className="navbar__link" to="/categoria/casa">
+                Casa
+              </Link>
+            </li>
+            <li className="navbar__item">
+              <Link className="navbar__link" to="/categoria/pet">
+                Pet
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 }
