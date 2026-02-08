aaebcdbbb3eb24a5cc638fc456236a25062b0f26
TrveLeo <leandro.baldan.ferreira@gmail.com>
2026-02-08 13:06:14 -0300

    Update go.py

--- api/routers/go.py
+++ api/routers/go.py
@@ -43,7 +43,6 @@ def go_to_affiliate(
         ip=request.client.host if request.client else None,
         user_agent=request.headers.get("user-agent"),
     )
-    click_repo.close()
 
     return RedirectResponse(
         url=row["url_afiliada"],
