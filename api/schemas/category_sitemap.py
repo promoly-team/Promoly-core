dab843a2de06e5e07af4ee323c16141eff956236
TrveLeo <leandro.baldan.ferreira@gmail.com>
2026-02-16 16:10:02 -0300

    Update category_sitemap.py

--- api/schemas/category_sitemap.py
+++ api/schemas/category_sitemap.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class CategorySitemapOut(BaseModel):
     slug: str
-    updated_at: datetime | None = None
+    created_at: datetime | None = None
 
     class Config:
         from_attributes = True
